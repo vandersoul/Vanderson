b00de23a8070e448df1f3bb67d1aadc1689e0435
vandersoul <111820309+vandersoul@users.noreply.github.com>
2024-04-09 09:56:03 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/dados.js
@@ -0,0 +1,4 @@
+let nome =prompt("qual é seu nome?");
+let sobreNome = prompt("qual seu sobre nome?");
+let idade = prompt("qual sua idade?");
+alert(`seu nome é ${nome} ${sobreNome} e sua idade é ${idade}`);
